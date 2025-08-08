d3f2c5590155953a5ca08b141133a5bddf7ef7ac
Kayode Micheal <153414835+OlabodeKayodem@users.noreply.github.com>
2025-08-08 15:45:41 +0100

    Update class_Ib.R

--- class_Ib.R
+++ class_Ib.R
@@ -9,18 +9,18 @@ dir.create("scripts")
 dir.create("results")
 
 #Loading the data set
-patient_info <- read.csv("C:Users/USER/Downloads/Learnings/AI Biotechnology/AI_Omics_Internship_2025/Module_I/raw_data/patient_info.csv")
+patient_info <- read.csv("C:/Users/USER/Downloads/Learnings/AI Biotechnology/AI_Omics_Internship_2025/Module_I/raw_data/patient_info.csv")
 
 
-# Inspect the structure of the dataset using appropriate R functions
+# Inspect the structure of the data set using appropriate R functions
 
 View(patient_info)
 str(patient_info)
 
 # Create a new variable for smoking status as a binary factor
-Patient_info$gender <- as.factor(Patient_info$gender)
-Patient_info$diagnosis <- as.factor(Patient_info$diagnosis)
-Patient_info$smoker <- as.factor(Patient_info$smoker)
+patient_info$gender <- as.factor(Patient_info$gender)
+patient_info$diagnosis <- as.factor(Patient_info$diagnosis)
+patient_info$smoker <- as.factor(Patient_info$smoker)
 str(patient_info)
 
 # Create a new variable for smoking status as a binary factor
@@ -29,5 +29,5 @@ patient_info$smoking_status <- ifelse(patient_info$smoker == "Yes", 1, 0)
 patient_info$smoking_status <- as.factor(patient_info$smoking_status)
 str(patient_info)
 
-#Save the cleaned dataset in your clean_data folder with the name patient_info_clean.csv
+#Save the cleaned data set in your clean_data folder with the name patient_info_clean.csv
 write.csv(patient_info, file = "C:/Users/USER/Downloads/Learnings/AI Biotechnology/AI_Omics_Internship_2025/Module_I/Clean_data/patient_info_clean.csv", row.names = FALSE)
